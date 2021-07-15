21efcdb3598a21c723c913638e8cf466eaac4b80
Luis Fernando <luiscaute4@gmail.com>
2021-07-14 22:06:14 -0500

    decimocuarto video: excepciones snapshot

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -5,4 +5,23 @@ describe('Es hora de las instantaneas', () => {
     test('Snapshot', () => {
         expect(getCharacter(rick)).toMatchSnapshot();
     });
+
+    test('Siempre faltará la instantanea', () => {
+        const user = {
+            createAt: new Date(),
+            id: Math.floor(Math.random() * 20),
+        };
+        expect(user).toMatchSnapshot();
+    });
+
+    test('Tenemos una excepcion dentro del codigo', () => {
+        const user = {
+            id: Math.floor(Math.random() * 20),
+            name: "Luis Fernando Caute"
+        };
+        expect(user).toMatchSnapshot({
+            id: expect.any(Number)
+        });
+    });
+
 });
