7d7b6b15574f81a05d898f03047c831296b98f7f
Luis Fernando <luiscaute4@gmail.com>
2021-07-14 21:14:58 -0500

    decimosegundo video: aplicaciones de testing para Async:Await

--- __test__/async.test.js
+++ __test__/async.test.js
@@ -14,4 +14,16 @@ describe('Provar Async/Await', () => {
             expect(data.name).toEqual('Rick Sanchez');
         });
     });
+
+    test('Realizando una peticion a una api con error', async() => {
+        const apiError = 'http://httpstat.us/500';
+        const peticion = getdataFromApi(apiError);
+        await expect(peticion).rejects.toEqual(Error('Request failed with status code 500'));
+    });
+
+    test('Resuelve un Hola', async() => {
+        await expect(Promise.resolve('Hola')).resolves.toBe('Hola');
+        await expect(Promise.reject('Error')).rejects.toBe('Error');
+    });
+
 });
